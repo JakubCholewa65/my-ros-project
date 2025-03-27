a5535ad477e9f6a528e061100326a2fe563ea469
JakubCholewa65 <102166704+JakubCholewa65@users.noreply.github.com>
2025-03-27 11:54:10 +0100

    Add files via upload

---
+++ packages/my_package/src/my_publisher_node.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import os
+import rospy
+from std_msgs.msg import String
+from duckietown.dtros import DTROS, NodeType
+from duckietown_msgs.msg import Twist2DStamped
+
+
+# Twist command for controlling the linear and angular velocity of the frame
+VELOCITY = 0.3  # linear vel    , in m/s    , forward (+)
+OMEGA = 4.0     # angular vel   , rad/s     , counter clock wise (+)
+
+
+class TwistControlNode(DTROS):
+
+    def __init__(self, node_name):
+        # initialize the DTROS parent class
+        super(TwistControlNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
+        # static parameters
+        vehicle_name = os.environ['VEHICLE_NAME']
+        twist_topic = f"/{vehicle_name}/car_cmd_switch_node/cmd"
+        # form the message
+        self._v = VELOCITY
+        self._omega = OMEGA
+        # construct publisher
+        self._publisher = rospy.Publisher(twist_topic, Twist2DStamped, queue_size=1)
+
+    def run(self):
+        # publish 10 messages every second (10 Hz)
+        rate = rospy.Rate(10)
+        message = Twist2DStamped(v=self._v, omega=self._omega)
+        while not rospy.is_shutdown():
+            self._publisher.publish(message)
+            rate.sleep()
+
+    def on_shutdown(self):
+        stop = Twist2DStamped(v=0.0, omega=0.0)
+        self._publisher.publish(stop)
+
+if __name__ == '__main__':
+    # create the node
+    node = TwistControlNode(node_name='twist_control_node')
+    # run node
+    node.run()
+    # keep the process from terminating
+    rospy.spin()
+
+
+class MyPublisherNode(DTROS):
+
+    def __init__(self, node_name):
+        # initialize the DTROS parent class
+        super(MyPublisherNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
+        # static parameters
+        self._vehicle_name = os.environ['VEHICLE_NAME']
+        # construct publisher
+        self._publisher = rospy.Publisher('chatter', String, queue_size=10)
+
+    def run(self):
+        # publish message every 1 second (1 Hz)
+        rate = rospy.Rate(1)
+        message = f"Hello from {self._vehicle_name}!"
+        while not rospy.is_shutdown():
+            rospy.loginfo("Publishing message: '%s'" % message)
+            self._publisher.publish(message)
+            rate.sleep()
+
+if __name__ == '__main__':
+    # create the node
+    node = MyPublisherNode(node_name='my_publisher_node')
+    # run node
+    node.run()
+    # keep the process from terminating
+    rospy.spin()
